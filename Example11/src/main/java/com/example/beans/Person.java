e9c967d47d09bade49916166de01fd7c53034818
sharmaaman088 <90156384+sharmaaman088@users.noreply.github.com>
2025-01-30 21:51:01 -0500

    Add files via upload

---
+++ Example11/src/main/java/com/example/beans/Person.java
@@ -0,0 +1,32 @@
+package com.example.beans;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Person {
+
+    private String name = "Lucy" ;
+
+    @Autowired
+    public Person(Vehicle vehicle)
+    {
+        System.out.println("Person bean Created by Spring");
+        this.vehicle = vehicle;
+        }
+
+    private Vehicle vehicle;
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public Vehicle getVehicle() {
+        return vehicle;
+    }
+    public void setVehicle(Vehicle vehicle) {
+        this.vehicle = vehicle;
+    }
+}
+
---
+++ Example11/src/main/java/com/example/config/ProjectConfig.java
@@ -0,0 +1,12 @@
+package com.example.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+//DONE
+
+@Configuration
+@ComponentScan(basePackages = "com.example.beans")
+        public class ProjectConfig
+{
+}
+
