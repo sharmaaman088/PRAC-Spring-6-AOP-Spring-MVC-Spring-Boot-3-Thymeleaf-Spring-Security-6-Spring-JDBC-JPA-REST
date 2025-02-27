b07fae2be4f5ad12e07ac6372d1ec9d254eab22c
Aman <sharmaaman088@gmail.com>
2025-02-27 12:30:56 -0500

    Added example27 with all files

---
+++ example27/src/main/java/com/eazybytes/eazyschool/config/ProjectSecurityConfig.java
@@ -0,0 +1,33 @@
+package com.eazybytes.eazyschool.config;
+
+
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+public class ProjectSecurityConfig {
+
+
+    @Bean
+    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(auth -> auth.anyRequest().permitAll())
+                .formLogin(withDefaults())
+                .httpBasic(withDefaults());
+        return http.build();
+
+
+//        http
+//                .authorizeHttpRequests(auth -> auth.anyRequest().denyAll())
+//                .formLogin(withDefaults())
+//                .httpBasic(withDefaults());
+//        return http.build();
+    }
+
+}
